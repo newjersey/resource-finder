1baf52eb74eabb83e7dce4d882cbd60e9c3a035f
Dave Cole <170641+dhcole@users.noreply.github.com>
2023-05-31 14:28:05 -0400

    Fix date offset issue

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -58,7 +58,7 @@ const Item = (props) => {
                         field[1].match(
                           /^([0-9]{4})-?(1[0-2]|0[1-9])-?(3[01]|0[1-9]|[12][0-9])$/
                         ) ? (
-                        new Date(field[1]).toLocaleDateString("en-US")
+                        field[1]
                       ) : field[1] &&
                         field[1].length !== 5 &&
                         field[1].match(
